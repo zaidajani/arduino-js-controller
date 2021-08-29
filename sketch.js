bb5f3d29c11e782cb8a4540874695f4aad7cd344
zaidajani <zaidajani2007@gmail.com>
2021-08-29 14:00:00 +0100

    some commit

---
+++ sketch.js
@@ -0,0 +1,19 @@
+const { Board, Led, Sensor } = require("johnny-five");
+const loudness = require("loudness");
+const brightness = require("brightness");
+const board = new Board();
+
+board.on("ready", () => {
+  const volMeter = new Sensor({ pin: "A3", threshold: 5 });
+  const brightMeter = new Sensor({ pin: "A5", threshold: 5 });
+
+  volMeter.on("change", (data) => {
+    const val = volMeter.scaleTo(0, 100);
+    loudness.setVolume(val);
+  });
+
+  brightMeter.on("change", (data) => {
+    console.log(volMeter.fscaleTo(0, 1));
+    brightness.set(volMeter.fscaleTo(0, 1));
+  });
+});
